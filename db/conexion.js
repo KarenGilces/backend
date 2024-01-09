f8ba8b4a5756d1f10a2e78dfeffd71f6dce4a2b8
KarenGilces <andreittam@outlook.com>
2024-01-09 16:59:09 -0500

    ultimooollllllllllll

--- db/conexion.js
+++ db/conexion.js
@@ -4,6 +4,8 @@ console.log(process.env.DB_USERNAME);
 console.log(process.env.DB_PASSWORD);
 console.log(process.env.DB_HOST);
 console.log(process.env.DB_DATABASE);
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const sequelize = new Sequelize(
     process.env.DB_DATABASE,
