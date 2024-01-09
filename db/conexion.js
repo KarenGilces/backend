1d743bfa60b69a0050b62c4cf409948389c7e581
KarenGilces <andreittam@outlook.com>
2024-01-09 16:53:44 -0500

    ultimooossssssssss

--- db/conexion.js
+++ db/conexion.js
@@ -1,7 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+import { Sequelize } from 'sequelize';
 console.log(process.env.DB_CONNECTION);
 console.log(process.env.DB_USERNAME);
 console.log(process.env.DB_PASSWORD);
