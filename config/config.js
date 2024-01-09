548009eb438ebbd1f4ad104d3bc52d7d6875712b
KarenGilces <andreittam@outlook.com>
2024-01-09 16:36:44 -0500

    ultimooosss

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// Cargar variables de entorno desde el archivo .env
+// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
 // Obtener las variables de entorno requeridas
@@ -12,11 +12,6 @@ const {
   DB_HOST,
   DB_DATABASE,
 } = process.env;
-
-// Validar que todas las variables de entorno requeridas estén presentes
-if (!DB_CONNECTION || !DB_USERNAME || !DB_PASSWORD || !DB_HOST || !DB_DATABASE) {
-  throw new Error('Faltan variables de entorno requeridas para la conexión a la base de datos.');
-}
 
 // Crear instancia de Sequelize usando las variables de entorno
 export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
