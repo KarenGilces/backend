d6942da60036c549cc4c0442cb9843ba4e157972
KarenGilces <andreittam@outlook.com>
2024-01-09 15:03:48 -0500

    ssszzz

--- db/conexion.js
+++ db/conexion.js
@@ -1,18 +1,22 @@
 import { Sequelize } from 'sequelize';
-import {DB_CONNECTION, DB_USERNAME, DB_PASSWORD,DB_HOST,DB_DATABASE} from '../config/config.js';
-// Option 3: Passing parameters separately (other dialects)
-export const sequelize = new Sequelize(
-    DB_DATABASE,
-    DB_USERNAME,
-    DB_PASSWORD,
-    {
-        host: DB_HOST,
-        dialect: 'mysql' // Asegúrate de tener el dialecto correcto para tu base de datos
+import { DB_CONNECTION, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DATABASE } from '../config/config.js';
+
+const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: DB_CONNECTION,
+    // Other options like port, logging, etc., can be added here
+});
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-);
+}
 
-
-
+export default sequelize;
 
 
 
