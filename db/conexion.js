423bf2c964714c18bd14365b2ab97284d2375fad
KarenGilces <andreittam@outlook.com>
2024-01-09 14:02:02 -0500

    gitignss

--- db/conexion.js
+++ db/conexion.js
@@ -1,25 +1,7 @@
 import { Sequelize } from 'sequelize';
-import {
-    DB_CONNECTION,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_DATABASE
-} from '../config/config.js';
+import { DB_CONNECTION, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DATABASE } from '../config/config.js';
 
 export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: DB_CONNECTION // Aquí se especifica el dialecto (en este caso, MySQL)
+    dialect: DB_CONNECTION // Asegúrate de que DB_CONNECTION tenga el valor correcto (mysql, postgres, etc.)
 });
-
-
-
-
-
-
-
-
-
-
-
-
