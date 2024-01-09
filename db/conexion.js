6bc8fb4a8f2b4c542a48bfffd619297e9dad4047
KarenGilces <andreittam@outlook.com>
2024-01-09 15:19:56 -0500

    sssssh

--- db/conexion.js
+++ db/conexion.js
@@ -1,26 +1,15 @@
 import { Sequelize } from 'sequelize';
-import { DB_CONNECTION, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DATABASE } from '../config/config.js';
-
-const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: DB_CONNECTION, // Specify the dialect explicitly (e.g., 'mysql')
-    // Other options like port, logging, etc., can be added here
-});
-
-// Test the connection
-async function testConnection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+import {DB_CONNECTION, DB_USERNAME, DB_PASSWORD,DB_HOST,DB_DATABASE} from '../config/config.js';
+// Option 3: Passing parameters separately (other dialects)
+export const sequelize = new Sequelize(
+    DB_DATABASE,
+    DB_USERNAME,
+    DB_PASSWORD,
+    {
+        host: DB_HOST,
+        dialect: DB_CONNECTION
     }
-}
-
-export { sequelize }; // Export the sequelize instance
-
-// Additionally, you can export the testConnection function if needed
-export { testConnection };
+);
 
 
 
