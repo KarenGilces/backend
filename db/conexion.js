e867ae251bd1f29fe11e67854bd06f357a40829d
KarenGilces <andreittam@outlook.com>
2024-01-09 16:06:16 -0500

    sssssddddddddddddddddd

--- db/conexion.js
+++ db/conexion.js
@@ -7,9 +7,10 @@ export const sequelize = new Sequelize(
     DB_PASSWORD,
     {
         host: DB_HOST,
-        dialect: DB_CONNECTION || 'mysql' // Establece un valor predeterminado si DB_CONNECTION no está definido
+        dialect: DB_CONNECTION
     }
 );
+
 
 
 
