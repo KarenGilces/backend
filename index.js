f48f60092fcd1e21fdb8888c61e4b1de5de97648
KarenGilces <andreittam@outlook.com>
2024-01-09 13:55:58 -0500

    hola1!s

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ import  routerColor from './router/ColorRouter.js';
 
 import { sequelize } from "./db/conexion.js";
 
-const _PORT = PORT || 3000;
+const _PORT = process.env.PORT ;
 const app = express();
 import path from 'path';
 app.use(express.json());
